e862772032994868465213f9ec872e4354a5418a
Paul Henrich <paul@henrich.me>
2016-01-18 12:16:18 -0500

    basic types

--- basic-types/with-scope.exs
+++ basic-types/with-scope.exs
@@ -1,11 +1,11 @@
 content = "Now is the time"
-lp = with {:ok, file} = File.open("/etc/passwd"),
-          content = IO.read(file, :all),
-          :ok  = File.close(file),
-          [_, uid, guid] <- Regex.run(~r{xxx:.*?:(\d+):(\d+)}, content)
-     do
-        "Group: #{guid}, user: #{uid}"
-        end
-        
+lp =
+  with {:ok, file} = File.open("/etc/passwd"),
+       content = IO.read(file, :all),
+       :ok  = File.close(file),
+       [_, uid, guid] <- Regex.run(~r{xxx:.*?:(\d+):(\d+)}, content)
+  do
+    "Group: #{guid}, user: #{uid}"
+  end
 IO.puts lp
 IO.puts content
